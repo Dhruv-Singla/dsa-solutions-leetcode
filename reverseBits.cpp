9c71140799e529ebb65b5b4cae16cec1743cb9d6
Dhruv Singla <143584848+Dhruv-Singla@users.noreply.github.com>
2024-10-20 20:50:20 +0530

    Add files via upload

---
+++ reverseBits.cpp
@@ -0,0 +1,11 @@
+long reverseBits(long n) {
+    long reversed = 0;
+    for (int i = 0; i < 32; i++) {
+        reversed <<= 1;
+        reversed |= (n & 1);
+        n >>= 1;
+    }
+    return reversed;
+}
+
+
