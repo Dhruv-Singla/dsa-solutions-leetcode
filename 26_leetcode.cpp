6f55c155dc0547d44e998871f719ee799efe7cc3
Dhruv Singla <143584848+Dhruv-Singla@users.noreply.github.com>
2025-01-27 16:03:11 +0530

    Create 26_leetcode.cpp

---
+++ 26_leetcode.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j=0,k=0;
+        for(int i=0;i<nums.size();i++){
+            while(nums[i]>nums[j]){
+                j++;
+                swap(nums[j],nums[i]);
+            }
+        }
+        return j+1;
+    }
+};
