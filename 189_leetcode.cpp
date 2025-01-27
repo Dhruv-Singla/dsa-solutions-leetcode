3acb26978e59ff7b2220d62173fb2cd89be7b7c2
Dhruv Singla <143584848+Dhruv-Singla@users.noreply.github.com>
2025-01-27 16:43:37 +0530

    Create 189_leetcode.cpp

---
+++ 189_leetcode.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        reverse(nums.begin(), nums.end()-k);
+        reverse(nums.rbegin(), nums.rbegin()+k);
+        reverse(nums.begin(), nums.end());
+    }
+};
