2ff7694169c855d5dd642550094463fb6b8d26ae
Dhruv Singla <143584848+Dhruv-Singla@users.noreply.github.com>
2025-01-27 15:41:23 +0530

    Create 1752_leetcode.cpp

---
+++ 1752_leetcode.cpp
@@ -0,0 +1,20 @@
+//beats 100% 
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int i = 0, sum = 0;
+        bool b = true;
+        while (i < nums.size() && nums[i] != 1) {
+            i++;
+        }
+        if (i == nums.size()) {
+            b = false;
+        }
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[(i + j)%nums.size()]>nums[(i + j + 1) % nums.size()]) {
+                sum++;
+            }
+        }
+        return (sum <= 1);
+    }
+};
