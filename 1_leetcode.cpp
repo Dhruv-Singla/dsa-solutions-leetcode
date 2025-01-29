da234a922d7e9e57756707e26b2d405914d8b6c6
Dhruv Singla <143584848+Dhruv-Singla@users.noreply.github.com>
2025-01-29 19:06:54 +0530

    Update 1_leetcode.cpp

--- 1_leetcode.cpp
+++ 1_leetcode.cpp
@@ -15,3 +15,13 @@ public:
     }
 };
 //1. Two Sum
+
+// Intuition
+// using 2 pointer but used map to store and find()
+
+// Approach
+// calculate diff to reach target and find that number in array by find()
+
+// Complexity
+// Time complexity: O(n))
+// Space complexity: O(n)
