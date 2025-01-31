d4d5be9a3a81e9012dcec7dd1b02b80adc6f4312
Dhruv Singla <143584848+Dhruv-Singla@users.noreply.github.com>
2025-01-31 20:45:34 +0530

    Update 53_leetcode.cpp

--- 53_leetcode.cpp
+++ 53_leetcode.cpp
@@ -18,3 +18,13 @@ public:
     }
 };
 //53. Maximum Subarray
+
+// Intuition
+// First I thought of two loop but time complexity was very high, then i though of removing elements which are decreasing the sum and starting ubarray after sum come down to less than zero.
+
+// Approach
+// I looped the array once and also iterate with counting sum, and when sum goes down to zero remove that length
+
+// Complexity
+// Time complexity: O(n)
+// Space complexity: O(1)
