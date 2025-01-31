c455093dbbb4639755c0aa974757baecd6fe5283
Dhruv Singla <143584848+Dhruv-Singla@users.noreply.github.com>
2025-01-31 21:22:31 +0530

    Create 126.cpp

---
+++ 126.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min=0;
+        int max=0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]-prices[min]>max){
+                max=prices[i]-prices[min];
+            }else if(prices[min]>prices[i]){
+                min=i;
+            }
+        }
+        return max;
+    }
+};
